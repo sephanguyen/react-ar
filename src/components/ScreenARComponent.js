de29e4cd5fbbf0c967f4d7e05fe91af36c485268
Pha Nguyen <sephanguyen@gmail.com>
2019-10-15 21:56:05 +0700

    update

--- src/components/ScreenARComponent.js
+++ src/components/ScreenARComponent.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
+import { AFrameRenderer, Marker } from 'react-web-ar';
 export default class ScreenARComponent extends Component {
   componentDidMount() {}
 
   render() {
     return (
-      <div>
-        <a-scene
-          stats
-          embedded
-          arjs="trackingMethod: best; debugUIEnabled: false"
-        >
-          <a-assets>
-            <a-asset-item id="fish-obj" src="/models/fish-2.obj"></a-asset-item>
-            <a-asset-item id="fish-mtl" src="/models/fish-2.mtl"></a-asset-item>
-          </a-assets>
-          <a-marker preset="hiro">
-            <a-entity obj-model="obj: #fish-obj; mtl: #fish-mtl"></a-entity>
-          </a-marker>
-          <a-entity camera></a-entity>
-        </a-scene>
-      </div>
+      <AFrameRenderer inherent={true}>
+        <Marker parameters={{ preset: 'hiro' }}>
+          <a-box color="blue" position="0 0.09 0" scale="0.4 0.8 0.8">
+            <a-animation
+              attribute="rotation"
+              to="360 0 0"
+              dur="2000"
+              easing="linear"
+              repeat="indefinite"
+            />
+          </a-box>
+        </Marker>
+      </AFrameRenderer>
     );
   }
 }

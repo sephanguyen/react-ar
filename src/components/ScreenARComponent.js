666bb8696a20dd07130528ea38359b1f16b24ee8
Pha Nguyen <sephanguyen@gmail.com>
2019-10-15 20:45:50 +0700

    update

--- src/components/ScreenARComponent.js
+++ src/components/ScreenARComponent.js
@@ -1,56 +1,22 @@
 import React, { Component } from 'react';
-import {
-  Scene,
-  AmbientLight,
-  Camera,
-  WebGLRenderer,
-  Color,
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial
-} from 'three';
-import { AFrameRenderer, Marker } from 'react-web-ar';
+
 export default class ScreenARComponent extends Component {
-  componentDidMount() {
-    // // === THREE.JS CODE START ===
-    // var scene = new Scene();
-    // let ambientLight = new AmbientLight(0xcccccc, 0.5);
-    // scene.add(ambientLight);
-    // let camera = new Camera();
-    // scene.add(camera);
-    // var renderer = new WebGLRenderer();
-    // renderer.setClearColor(new Color('lightgrey'), 0);
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // renderer.domElement.style.position = 'absolute';
-    // renderer.domElement.style.top = '0px';
-    // renderer.domElement.style.left = '0px';
-    // this.mount.appendChild(renderer.domElement);
-    // var geometry = new BoxGeometry(1, 1, 1);
-    // var material = new MeshBasicMaterial({ color: 0x00ff00 });
-    // var cube = new Mesh(geometry, material);
-    // scene.add(cube);
-    // camera.position.z = 5;
-    // var animate = function() {
-    //   requestAnimationFrame(animate);
-    //   cube.rotation.x += 0.01;
-    //   cube.rotation.y += 0.01;
-    //   renderer.render(scene, camera);
-    // };
-    // animate();
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div>
-        <a-scene embedded arjs="sourceType: webcam;">
+        <a-scene
+          stats
+          embedded
+          arjs="trackingMethod: best; debugUIEnabled: false"
+        >
           <a-assets>
-            <a-asset-item
-              id="smiley"
-              src="models/SmilingFace.gltf"
-            ></a-asset-item>
+            <a-asset-item id="fish-obj" src="/models/fish-2.obj"></a-asset-item>
+            <a-asset-item id="fish-mtl" src="/models/fish-2.mtl"></a-asset-item>
           </a-assets>
-          <a-marker preset="hiro">
-            <a-box position="0 0.5 0" material="color: red;"></a-box>
+          <a-marker type="pattern" url="data/hiro.patt">
+            <a-entity obj-model="obj: #fish-obj; mtl: #fish-mtl"></a-entity>
           </a-marker>
           <a-entity camera></a-entity>
         </a-scene>

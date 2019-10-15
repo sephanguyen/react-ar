f106561b8e45af1f306c113091816d53455953ef
Pha Nguyen <sephanguyen@gmail.com>
2019-10-15 21:42:57 +0700

    update

--- src/components/ScreenARComponent.js
+++ src/components/ScreenARComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 export default class ScreenARComponent extends Component {
   componentDidMount() {}
 
@@ -15,7 +14,7 @@ export default class ScreenARComponent extends Component {
             <a-asset-item id="fish-obj" src="/models/fish-2.obj"></a-asset-item>
             <a-asset-item id="fish-mtl" src="/models/fish-2.mtl"></a-asset-item>
           </a-assets>
-          <a-marker type="pattern" url="data/hiro.patt">
+          <a-marker preset="hiro">
             <a-entity obj-model="obj: #fish-obj; mtl: #fish-mtl"></a-entity>
           </a-marker>
           <a-entity camera></a-entity>

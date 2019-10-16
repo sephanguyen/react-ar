d7df2b2a084d9ef6f30a8a7469849957cd3b2b36
Pha Nguyen <sephanguyen@gmail.com>
2019-10-16 08:34:29 +0700

    update component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
-// import SphereComponent from './components/SphereComponent';
+import SphereComponent from './components/SphereComponent';
 import ScreenARComponent from './components/ScreenARComponent';
 
 function App() {
-  return <ScreenARComponent></ScreenARComponent>;
+  return (
+    <div>
+      <ScreenARComponent></ScreenARComponent>
+      <button class="ui btn-bottom">OPEN 360 IMAGE</button>
+      {/* <SphereComponent></SphereComponent> */}
+    </div>
+  );
 }
 
 export default App;
--- src/components/ScreenARComponent.js
+++ src/components/ScreenARComponent.js
@@ -5,25 +5,12 @@ import { AFrameRenderer, Marker } from 'react-web-ar';
 export default class ScreenARComponent extends Component {
   componentDidMount() {}
 
-  // <div>
-  //       <a-scene
-  //         stats
-  //         embedded
-  //         arjs="trackingMethod: best; debugUIEnabled: false"
-  //       >
-  //         <a-assets>
-  //           <a-asset-item id="fish-obj" src="/models/fish-2.obj"></a-asset-item>
-  //           <a-asset-item id="fish-mtl" src="/models/fish-2.mtl"></a-asset-item>
-  //         </a-assets>
-  //         <a-marker preset="hiro">
-  //           <a-entity obj-model="obj: #fish-obj; mtl: #fish-mtl"></a-entity>
-  //         </a-marker>
-  //         <a-entity camera></a-entity>
-  //       </a-scene>
-  //     </div>
   render() {
     return (
-      <AFrameRenderer inherent={true}>
+      <AFrameRenderer
+        embedded
+        arjs="sourceType: webcam; debugUIEnabled: false;"
+      >
         <a-assets>
           <a-asset-item id="fish-obj" src="/models/fish-2.obj"></a-asset-item>
           <a-asset-item id="fish-mtl" src="/models/fish-2.mtl"></a-asset-item>
